f15b3dcef6a6dbffde6cd84dd78a4f25dcf1c214
Dane <golightning225@gmail.com>
2024-11-05 11:49:18 +0800

    reduced image file sizes

--- src/components/CLife.jsx
+++ src/components/CLife.jsx
@@ -3,42 +3,42 @@ import './clife.css';
 const clifeImages = [
   {
     id: 1,
-    url: '../images/Arts 1.png',
+    url: '../images/Arts 1.jpg',
     title: 'Campus Life 1',
   },
   {
     id: 2,
-    url: '../images/Arts 2.png',
+    url: '../images/Arts 2.jpg',
     title: 'Campus Life 2',
   },
   {
     id: 3,
-    url: '../images/CWTS 1.png',
+    url: '../images/CWTS 1.jpg',
     title: 'Campus Life 3',
   },
   {
     id: 4,
-    url: '../images/CWTS 2.png',
+    url: '../images/CWTS 2.jpg',
     title: 'Campus Life 4',
   },
   {
     id: 5,
-    url: '../images/Instruments 1.png',
+    url: '../images/Instruments 1.jpg',
     title: 'Campus Life 5',
   },
   {
     id: 6,
-    url: '../images/Instruments 2.png',
+    url: '../images/Instruments 2.jpg',
     title: 'Campus Life 6',
   },
   {
     id: 7,
-    url: '../images/ROTC 1.png',
+    url: '../images/ROTC 1.jpg',
     title: 'Campus Life 7',
   },
   {
     id: 8,
-    url: '../images/ROTC 2.png',
+    url: '../images/ROTC 2.jpg',
     title: 'Campus Life 8',
   },
 ];
